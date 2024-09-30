76f2c657bc257a5e831d533739cc2e59d912b15f
chenze <chenze>
2024-09-30 17:07:55 +0800

    try to log authorize

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,24 +1,37 @@
 // Import the SDK
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { DiscordSDK } from "@discord/embedded-app-sdk";
 
 import "./main.css";
 import rocketLogo from '/rocket.png';
 
 // Instantiate the SDK
-/*const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+let discordSdk:DiscordSDK = null;
+
+async function setupDiscordSdk() {
+  discordSdk = new DiscordSDK("1283000047155544066")
+  await discordSdk.ready();
+  let ret = await discordSdk.commands.authorize({
+    client_id:discordSdk.clientId,
+    response_type:"code",
+    state: "",
+    scope:[
+    ],
+  });
+  console.log("code:"+ret);
+}
 
 setupDiscordSdk().then(() => {
   console.log("Discord SDK is ready");
 });
 
-async function setupDiscordSdk() {
-  await discordSdk.ready();
-}*/
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <img src="${rocketLogo}" class="logo" alt="Discord" />
-    <h1>Hello, World!</h1>
-  </div>
-`;
-
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <div>
+      <img src={rocketLogo} className="logo" alt="Discord" />
+      <h1>Hello, World!</h1>
+    </div>
+  </React.StrictMode>
+)
